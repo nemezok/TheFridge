a280ad96914ac6792acb04cff873a9d0ee65c75a
Stanislav Gerlinger <stas0904@mail.ru>
2018-07-02 22:50:51 +0400

    Добавили цикл в Product

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -6,51 +6,12 @@
 				<a href="" class="btn1 addproduct"><i class="fo plus"></i><span>Добавить продукты</span></a>
 			</div>
 			<div class="product-list flex flex-row">
-				<div class="product-wrapper" md-flex="4" sm-flex="6">
+				<div class="product-wrapper" md-flex="4" sm-flex="6" v-for="prod in products">
 					<div class="product expired"> <!-- Классы: epxired, expires-soon -->
 						<div class="info" xs-flex="9">
-							<h4 class="title">Название продукта</h4>
-							<div class="quantity"><span class="amount">15,8</span><span class="measure">кг</span></div>
-							<div class="expiration"><i class="fo clock"></i><span class="date">10 июля 2018</span></div>
-						</div>
-						<div class="item-manage" xs-flex="3">
-							<a href="" class="edit"><i class="fo pencil"></i></a>
-							<a href="" class="delete"><i class="fo trash"></i></a>
-						</div>
-					</div>
-				</div>
-				<div md-flex="4" sm-flex="6">
-					<div class="product expired">
-						<div class="info" xs-flex="9">
-							<h4 class="title">Название продукта</h4>
-							<div class="quantity"><span class="amount">15,8</span><span class="measure">кг</span></div>
-							<div class="expiration"><i class="fo clock"></i><span class="date">10 июля 2018</span></div>
-						</div>
-						<div class="item-manage" xs-flex="3">
-							<a href="" class="edit"><i class="fo pencil"></i></a>
-							<a href="" class="delete"><i class="fo trash"></i></a>
-						</div>
-					</div>
-				</div>
-				<div md-flex="4" sm-flex="6">
-					<div class="product expires-soon">
-						<div class="info" xs-flex="9">
-							<h4 class="title">Название продукта</h4>
-							<div class="quantity"><span class="amount">15,8</span><span class="measure">кг</span></div>
-							<div class="expiration"><i class="fo clock"></i><span class="date">10 июля 2018</span></div>
-						</div>
-						<div class="item-manage" xs-flex="3">
-							<a href="" class="edit"><i class="fo pencil"></i></a>
-							<a href="" class="delete"><i class="fo trash"></i></a>
-						</div>
-					</div>
-				</div>
-				<div md-flex="4" sm-flex="6">
-					<div class="product">
-						<div class="info" xs-flex="9">
-							<h4 class="title">Название продукта</h4>
-							<div class="quantity"><span class="amount">15,8</span><span class="measure">кг</span></div>
-							<div class="expiration"><i class="fo clock"></i><span class="date">10 июля 2018</span></div>
+							<h4 class="title">{{prod.title}}</h4>
+							<div class="quantity"><span class="amount">{{prod.amount}}</span><span class="measure">{{prod.measure}}</span></div>
+							<div class="expiration"><i class="fo clock"></i><span class="date">{{prod.expiration}}</span></div>
 						</div>
 						<div class="item-manage" xs-flex="3">
 							<a href="" class="edit"><i class="fo pencil"></i></a>
@@ -59,7 +20,7 @@
 					</div>
 				</div>
 			</div>
-		</div>	
+		</div>
 	</section>
 </template>
 
@@ -68,13 +29,21 @@ import firebase from 'firebase'
 export default {
   name: 'Products',
   mounted () {
-    const productRef = firebase.database().ref('Product')
+    const productRef = firebase.database().ref('Products')
     console.log(productRef)
     productRef.once('value').then(this.GetName).catch((error) => console.log(error))
   },
+
+  data: function () {
+    return {
+      products: {}
+    }
+  },
+
   methods: {
     GetName: function (snapshot) {
-      console.log(snapshot.val())
+      this.products = snapshot.val()
+      return this.products
     }
   }
 }
