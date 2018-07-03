a8938924debd69096a86f9010368c80e0ec7e505
Eugene <senfemaka@gmail.com>
2018-07-03 19:55:29 +0400

    добавил фильтры

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -3,20 +3,22 @@
 		<section id="recipe-catalog">
 				<div class="container">Каталог рецептов
 					<form action="" method="get" class="recipe-list-filter flex flex-row flex-center">
-						<div class="search"><input name="search" type="text" value="" placeholder="Искать"></div>
-						<div class="time" xs-flex="8"><input name="cookingtime" type="text" value="" placeholder="Время приготовления"></div>
-						<div class="reset" xs-flex="4"><a href="" class="btn1">Сброс</a></div>
+						<div class="search"><input name="search" type="text" value="" placeholder="Искать" v-model='searchNamePattern'></div>
+						<div class="time" xs-flex="8"><input name="cookingtime" type="text" value="" placeholder="Время приготовления" v-model='searchTimePattern'></div>
+						<div class="reset" xs-flex="4"><a href="" class="btn1" @click.prevent="resetPattern">Сброс</a></div>
 					</form>
 					<div class="recipe-list flex flex-row">
-						<div class="recipe-wrapper" md-flex="4" sm-flex="6" v-for="i in 50">
+						<div class="recipe-wrapper" md-flex="4" sm-flex="6" v-for="(rcp, id) in reciepts" v-if="matchTitle(rcp.title) && matchTime(rcp.cooking_time)">
 							<div class="recipe"> <!-- Классы: epxired, expires-soon -->
 								<div class="info" xs-flex="9">
-									<h4 class="title">Название блюда</h4>
-									<div class="cooking-time"><i class="fo clock"></i><span class="time">30 минут</span></div>
+									<router-link :to="{ name:'details', params: {id: rcp.ID} }">
+										<h4 class="title">{{rcp.title}}</h4>
+										<div class="cooking-time"><i class="fo clock"></i><span class="time">{{rcp.cooking_time}} мин.</span></div>
+									</router-link>
 								</div>
 								<div class="item-manage" xs-flex="3">
 									<a href="" class="edit"><i class="fo pencil"></i></a>
-									<a href="" class="delete"><i class="fo trash"></i></a>
+									<a href="" class="delete" @click.prevent="removeReciept(rcp)"><i class="fo trash"></i></a>
 								</div>
 							</div>
 						</div>
@@ -27,9 +29,45 @@
 </template>
 
 <script>
-	export default {
-	  name: 'Calendar'
-	}
+
+import store from '../store/store'
+
+export default {
+
+  name: 'Calendar',
+
+  data () {
+    return {
+      searchNamePattern: '',
+      searchTimePattern: ''
+    }
+  },
+
+  computed: {
+    reciepts () {
+      return store.getters.Reciepts
+    }
+  },
+
+  methods: {
+    matchTitle (n) {
+      if (this.searchNamePattern === '') return true
+      if (n.match(this.searchNamePattern) != null) return true
+      return false
+    },
+
+    matchTime (n) {
+      if (this.searchTimePattern === '') return true
+      if (this.searchTimePattern <= n) return false
+      return true
+    },
+
+    resetPattern () {
+      this.searchNamePattern = ''
+      this.searchTimePattern = ''
+    }
+  }
+}
 </script>
 
 <style src="../assets/style.css">
